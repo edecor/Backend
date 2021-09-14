6c8477f887793f05d426068c72e18acdd12f7d6d
FiazAbrar <abrarfiaz.bd@gmail.com>
2021-09-14 22:11:15 +0600

    define local static root

--- core/settings/local.py
+++ core/settings/local.py
@@ -27,9 +27,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
 MEDIA_URL = "/media/"
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
