2adeeb531d3d42b8a1ad21fe5452b2b2cd887d99
FiazAbrar <abrarfiaz.bd@gmail.com>
2021-09-15 14:15:37 +0600

    if env is prod, dont run vanilla db connect check

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,21 @@ set -o nounset
 
 readonly cmd="$*"
 
-postgres_ready () {
-  # Check that postgres is up and running on port `5432`:
-  dockerize -wait 'tcp://db:5432' -timeout 5s
-}
+if [ "$DJANGO_ENV" != 'production' ]; then
+  postgres_ready () {
+    # Check that postgres is up and running on port `5432`:
+    dockerize -wait 'tcp://db:5432' -timeout 5s
+  }
 
-# We need this line to make sure that this container is started
-# after the one with postgres:
-until postgres_ready; do
-  >&2 echo 'Postgres is unavailable - sleeping'
-done
+  # We need this line to make sure that this container is started
+  # after the one with postgres:
+  until postgres_ready; do
+    >&2 echo 'Postgres is unavailable - sleeping'
+  done
 
-# It is also possible to wait for other services as well: redis, elastic, mongo
->&2 echo 'Postgres is up - continuing...'
+  # It is also possible to wait for other services as well: redis, elastic, mongo
+  >&2 echo 'Postgres is up - continuing...'
+fi
 
 # Evaluating passed command (do not touch):
 # shellcheck disable=SC2086
