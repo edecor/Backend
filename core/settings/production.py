1c6a83ba928affeabcd01ed3c797668f26ac7ea8
FiazAbrar <abrarfiaz.bd@gmail.com>
2021-09-27 11:25:05 +0600

    try

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     config("TEMP_EDECOR_DOMAIN"),
@@ -47,7 +47,8 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-SECURE_SSL_REDIRECT = True
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
@@ -74,8 +75,9 @@ AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
 AWS_DEFAULT_ACL = "public-read"
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+AWS_S3_CUSTOM_DOMAIN = "media.edecor.com.bd"
+
 
 # s3 static settings
 STATIC_URL = "https://media.edecor.com.bd/static/"
