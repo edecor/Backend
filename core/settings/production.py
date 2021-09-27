8fb2ccf3c747a5f38e888513847e39e4dfb08cd2
FiazAbrar <abrarfiaz.bd@gmail.com>
2021-09-27 08:26:52 +0600

    custom media url

--- core/settings/production.py
+++ core/settings/production.py
@@ -83,6 +83,5 @@ STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
 STATICFILES_STORAGE = "core.storage_backends.StaticStorage"
 
 # s3 public media settings
-PUBLIC_MEDIA_LOCATION = "media"
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
+MEDIA_URL = "https://media.edecor.com.bd/media/"
 DEFAULT_FILE_STORAGE = "core.storage_backends.PublicMediaStorage"
