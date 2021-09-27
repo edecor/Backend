df1359c3e9d0d4642738e502a802001082c7ba68
FiazAbrar <abrarfiaz.bd@gmail.com>
2021-09-27 07:14:27 +0600

    csp v2

--- core/settings/production.py
+++ core/settings/production.py
@@ -51,6 +51,12 @@ SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
+CSP_SCRIPT_SRC = ("'self'",)
+CSP_IMG_SRC = ("'self'",)
+CSP_FONT_SRC = ("'self'",)
+CSP_STYLE_SRC = ("'self'",)
+CSP_DEFAULT_SRC = ("'none'",)
+
 # ---------------- REST FRAMEWORK ----------------
 
 REST_FRAMEWORK = {
