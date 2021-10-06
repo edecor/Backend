9dfab5e052e487e5fa65dc773c6d6a9a5f12500f
FiazAbrar <abrarfiaz.bd@gmail.com>
2021-10-06 22:19:52 +0600

    api url for material

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,10 @@ from .views import MaterialListView, MaterialDetailView, tempOKview, trigger_err
 from .views import tempOKview, trigger_error
 
 urlpatterns = [
-    path("products/", MaterialListView.as_view(), name="product-list"),
-    path("products/<slug>", MaterialDetailView.as_view(), name="product-detail"),
+    path("products/materials", MaterialListView.as_view(), name="product-list"),
+    path(
+        "products/materials/<slug>", MaterialDetailView.as_view(), name="product-detail"
+    ),
     path("", tempOKview),
     path("sentry-debug/", trigger_error),
 ]
