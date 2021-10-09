a58629d579288d0e6e934384506a5a49069eb6ff
FiazAbrar <abrarfiaz.bd@gmail.com>
2021-10-09 22:01:25 +0600

    register every model

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Category, Material, ProductImage, Brand, Supplier
+from .models import (
+    Category,
+    Material,
+    ProductImage,
+    Brand,
+    Supplier,
+    BathroomProducts,
+    DecorationsProducts,
+    FabricTextileProducts,
+    FurnitureProducts,
+    HardwareToolProducts,
+    HomeApplianceProducts,
+    KitchenProducts,
+    LandscapeProducts,
+    LightProducts,
+    RugsMatFloorProducts,
+    SecurityProtectionProducts,
+)
 from ckeditor.widgets import CKEditorWidget
 from django.db import models
 from django_json_widget.widgets import JSONEditorWidget
@@ -51,4 +68,59 @@ class BrandAdmin(admin.ModelAdmin):
 
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BathroomProducts)
+class BathroomProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DecorationsProducts)
+class DecorationProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FabricTextileProducts)
+class FabricTextileProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FurnitureProducts)
+class FurnitureProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(HardwareToolProducts)
+class HardwareToolProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(HomeApplianceProducts)
+class HomeApplianceProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(KitchenProducts)
+class KitchenProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(LandscapeProducts)
+class LandscapeProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(LightProducts)
+class LightProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RugsMatFloorProducts)
+class RugsMatFloorProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SecurityProtectionProducts)
+class SecurityProtectionProductsAdmin(admin.ModelAdmin):
     pass
