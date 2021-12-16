256ee9762d1f57ed00f71b4a4e3c893c4eeab7e2
Abrar Fiaz <abrarfiaz.bd@gmail.com>
2021-12-17 04:44:24 +0600

    fix: material serializer images working now
    https://stackoverflow.com/questions/48719776/django-rest-framwork-got-attributeerror-when-attempting-to-get-a-value-for-field this was the exact problem happening

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Material, ProductImage, Category
+from .models import Material, ProductImage
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
@@ -8,15 +8,8 @@ class ProductImageSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ["id", "name"]
-
-
 class MaterialSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True)
-    material_type = CategorySerializer(many=True)
+    productimage_set = ProductImageSerializer(many=True)
 
     class Meta:
         model = Material
@@ -24,9 +17,8 @@ class MaterialSerializer(serializers.ModelSerializer):
             "uuid",
             "name",
             "slug",
-            "images",
-            "material_type",
             "description",
+            "productimage_set",
             "price",
             "available",
             "updated",
