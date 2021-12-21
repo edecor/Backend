6cc5e03e6a73b8b1be553c9bfd111b8d007ac1a2
Abrar Fiaz <abrarfiaz.bd@gmail.com>
2021-12-21 17:03:27 +0600

    feat: add uuid field for customer

--- user_profiles/models.py
+++ user_profiles/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.conf import settings
 
@@ -22,6 +24,7 @@ class Customer(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50, null=True, blank=True)
     last_name = models.CharField(max_length=50, null=True, blank=True)
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return self.user.email
