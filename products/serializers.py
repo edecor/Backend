116f3d9e20ced29372992f6d0b99167473be90c6
Abrar Fiaz <abrarfiaz.bd@gmail.com>
2021-12-24 10:55:30 +0600

    feat: don't expose nulls in image api

--- products/serializers.py
+++ products/serializers.py
@@ -1,11 +1,19 @@
+from collections import OrderedDict
+
 from rest_framework import serializers
 from .models import Material, ProductImage
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+        return OrderedDict(
+            [(key, result[key]) for key in result if result[key] is not None]
+        )
+
     class Meta:
         model = ProductImage
-        fields = "__all__"
+        fields = ["id", "image", "alt", "is_description_image"]
 
 
 class MaterialSerializer(serializers.ModelSerializer):
