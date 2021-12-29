1efb396b3772bb1b11038560af823d862e99f5fc
Abrar Fiaz <abrarfiaz.bd@gmail.com>
2021-12-29 12:18:16 +0600

    feat: rooms update permission classes and toprooms

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Room
 from .serializers import RoomSerializer
@@ -9,3 +10,10 @@ class RoomDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
     lookup_field = "name"
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class TopRoomsInHome(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
