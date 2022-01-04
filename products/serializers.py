4c28485bbc90e9a3f5e2fa97fedeec80241ce2fb
Abrar Fiaz <abrarfiaz.bd@gmail.com>
2022-01-05 01:57:13 +0600

    feat(comments): product api includes comments

--- products/serializers.py
+++ products/serializers.py
@@ -2,6 +2,24 @@ from collections import OrderedDict
 
 from rest_framework import serializers
 from .models import Material, ProductImage
+from user_profiles.models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+        return OrderedDict(
+            [(key, result[key]) for key in result if result[key] is not None]
+        )
+
+    class Meta:
+        model = Comment
+        fields = [
+            "customer",
+            "commentimage_set",
+            "comment_text",
+        ]
+        depth = 1
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
@@ -18,6 +36,7 @@ class ProductImageSerializer(serializers.ModelSerializer):
 
 class MaterialSerializer(serializers.ModelSerializer):
     productimage_set = ProductImageSerializer(many=True)
+    comments = CommentSerializer(many=True, source="comment_set")
 
     class Meta:
         model = Material
@@ -31,4 +50,5 @@ class MaterialSerializer(serializers.ModelSerializer):
             "available",
             "updated",
             "additional_fields",
+            "comments",
         ]
