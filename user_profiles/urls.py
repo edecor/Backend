e1c80a71a33aa63f9ae7e4cb00b83719bf321a8c
Abrar Fiaz <abrarfiaz.bd@gmail.com>
2022-01-05 00:52:13 +0600

    cleanup(comments): remove the temp test view in urls.py

--- user_profiles/urls.py
+++ user_profiles/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
-from django.http import HttpResponse
 
 from .views import CustomerProfileDetailView, WishListDetailView, CommentListView
-
-
-def testview(request):
-    return HttpResponse("hi")
-
 
 urlpatterns = [
     path("comments/", CommentListView.as_view(), name="comment_list"),
